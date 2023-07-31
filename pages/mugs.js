b70797e8710206e0e212c38e5508e4626dad25f0
poudyalamit <poudyalamit20@gmail.com>
2023-08-01 00:14:25 +0545

    added other pages

--- pages/mugs.js
+++ pages/mugs.js
@@ -1,9 +1,148 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 
 const Mugs = () => {
+  const router= useRouter();
   return (
     <div>
-      
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg cursor-pointer" onClick={() => router.push('/product/buyit')}>
+              <a className="block relative  rounded overflow-hidden">
+                <img alt="ecommerce" className=" md:h-[36vh] h-[30vh] block md:mx-0 m-auto" src="https://m.media-amazon.com/images/I/71ooX8CvTxL._AC_SX425_.jpg" />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Buy Perfect</h2>
+                <p className="mt-1"><b>Rs.</b> 499</p>
+                <p className='mt-l'>S, M, L, XXL</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
