73a51d9e17930baac906995742226865721fa733
poudyalamit <poudyalamit20@gmail.com>
2023-08-02 01:14:12 +0545

    added cart functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,25 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import {FaShoppingCart} from 'react-icons/fa'
+import React, { useRef } from 'react'
+import { FaShoppingCart } from 'react-icons/fa'
+import { BsFillBagCheckFill } from 'react-icons/bs'
+import { AiFillCloseCircle, AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai'
 
 const Navbar = () => {
+  const ref=useRef();
+  const toggleCart=()=>{
+    if (ref.current.classList.contains('translate-x-full')){
+      ref.current.classList.remove('translate-x-full')
+      ref.current.classList.add('translate-x-0')
+    }else if(!ref.current.classList.contains('translate-x-full')){
+      ref.current.classList.remove('translate-x-0')
+      ref.current.classList.add('translate-x-full')
+    }
+  }
   return (
     <div className='flex flex-col md:flex-row  md:justify-start justify-center items-center  py-2 shadow-md'>
       <div className="logo mx-5">
-        <Link href={'/'}><Image src={"/logo.png"} width={50} height={50} style={{ borderRadius: "50%"}} alt=''/></Link>
+        <Link href={'/'}><Image src={"/logo.png"} width={50} height={50} style={{ borderRadius: "50%" }} alt='' /></Link>
       </div>
       <div className="nav">
         <ul className='flex items-center space-x-6 font-bold md:text-md'>
@@ -17,8 +29,52 @@ const Navbar = () => {
           <Link legacyBehavior href={'/mugs'}><a><li>Mugs</li></a></Link>
         </ul>
       </div>
-      <div className="cart absolute right-0 mx-5 top-4">
-        <FaShoppingCart className='text-xl md:text-2xl'/>
+      <div onClick={toggleCart} className="cart absolute right-0 mx-5 top-4 cursor-pointer">
+        <FaShoppingCart className='text-xl md:text-2xl' />
+      </div>
+
+      <div ref={ref} className="w-72 h-full sideCart absolute top-0 right-0 bg-green-100 px-8 py-10 transform transition-transform translate-x-full ">
+        <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-green-600"><AiFillCloseCircle /></span>
+        <ol className='list-decimal font-semibold'>
+          <li>
+            <div className="item flex my-5">
+            <div className='w-2/3 font-semibold'>Tshirts</div>
+            <div className=' flex items-center font-semibold justify-center w-1/3 text-lg'>
+              <AiOutlineMinusCircle className='cursor-pointer text-green-800'/>
+              <span className='mx-2 text-sm'>1</span>
+              <AiOutlinePlusCircle className='cursor-pointer text-green-800'/></div>
+            </div>
+          </li>
+          <li >
+            <div className="item flex my-5">
+            <div className='w-2/3 font-semibold'>Tshirts</div>
+            <div className=' flex items-center font-semibold justify-center w-1/3 text-lg'>
+              <AiOutlineMinusCircle className='cursor-pointer text-green-800'/>
+              <span className='mx-2 text-sm'>1</span>
+              <AiOutlinePlusCircle className='cursor-pointer text-green-800'/></div>
+            </div>
+          </li>
+          <li >
+            <div className="item flex my-5">
+            <div className='w-2/3 font-semibold'>Tshirts</div>
+            <div className=' flex items-center font-semibold justify-center w-1/3 text-lg'>
+              <AiOutlineMinusCircle className='cursor-pointer text-green-800'/>
+              <span className='mx-2 text-sm'>1</span>
+              <AiOutlinePlusCircle className='cursor-pointer text-green-800'/></div>
+            </div>
+          </li>
+          <li >
+            <div className="item flex my-5">
+            <div className='w-2/3 font-semibold'>Tshirts</div>
+            <div className=' flex items-center font-semibold justify-center w-1/3 text-lg'>
+              <AiOutlineMinusCircle className='cursor-pointer text-green-800'/>
+              <span className='mx-2 text-sm'>1</span>
+              <AiOutlinePlusCircle className='cursor-pointer text-green-800'/></div>
+            </div>
+          </li>
+        </ol>
+        <button className="flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"><BsFillBagCheckFill className='m-1'/> Checkout</button>
       </div>
     </div>
   )
